14c35fc66ed4932131b6dd83d3f91e881e9e7836
Sandjar <sandjarkayumov@gmail.com>
2020-12-02 13:02:00 +0500

    zzz

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-const tryNowBtn = document.querySelector('.btn');
+// const tryNowBtn = document.querySelector('.btn');
+// window.addEventListener("scroll", function () {
+//   document.querySelector(".fa-arrow-up").innerHTML = pageYOffset + "px";
+//   console.log("Scrool");
+// });
 
-tryNowBtn.addEventListener("click", function () {
-  const modalWindow = document.querySelector(".modal");
-  modalWindow.classList.toggle("none");
-  console.log("Click");
-});
-const countBtn = document.querySelector(".btn");
-console.log(countBtn.classList);
+const btnMenu = document.querySelector(".menu__burger");
+const menuOpen = document.querySelector(".menu__open");
+const menuClose = document.querySelector(".menu__close");
 
-const leftSection = document.querySelector(".left__section");
-leftSection.addEventListener("click", function () {
-  if (leftSection) {
-    leftSection.style.width = "400px";
-  } else {
-    // leftSection.classList.remove(leftSection);
+
+btnMenu.addEventListener("click", function () {
+  if(btnMenu){
+    btnMenu.classList.add("menuClose")
   }
-  console.log(leftSection);
 
-})
-window.addEventListener("keyup", function (e) {
-  if (e.Escape == 27) {
-    leftSection.classList.add('none')
-  }
-});
 
-window.addEventListener("scroll", function () {
-  document.querySelector(".fa-arrow-up").innerHTML = pageYOffset + "px";
-  console.log("Scrool");
+
+
+
+  console.log("Menu");
 });
