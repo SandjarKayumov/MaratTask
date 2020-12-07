7096451edba8d32e54dc442db30e828e8d92eded
Sandjar <sandjarkayumov@gmail.com>
2020-12-07 11:52:10 +0500

    Added responsive

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
-// const tryNowBtn = document.querySelector('.btn');
 // window.addEventListener("scroll", function () {
 //   document.querySelector(".fa-arrow-up").innerHTML = pageYOffset + "px";
 //   console.log("Scrool");
 // });
 
-const btnMenu = document.querySelector(".menu__burger");
-const menuOpen = document.querySelector(".menu__open");
-const closeMenu = document.querySelector(".menu__close");
-const nav = document.querySelector(".nav");
+const bntBurger = document.querySelector(".menu__burger");
+const menuClose = document.querySelector(".menu__close");
+const faBars = document.querySelector(".fa-bars");
+const faTimes = document.querySelector(".fa-times");
+const navMedia = document.querySelector(".nav__media");
 
-btnMenu.addEventListener("click", function () {
-  nav.style.display ="block";
-  console.log("Menu");
-});
+const openMenu = function () {
+  navMedia.classList.add("block");
+  faBars.style.display = "block";
+  menuClose.style.display = "block";
+  console.log("Open");
+}
+const closeMenu = function () {
+  // navMedia.classList.remove("block");
+  // faTimes.style.display = "none";
+  // faBars.style.display = "block";
+  // console.log("Close");
+}
+
+bntBurger.addEventListener("click", openMenu);
+menuClose.addEventListener("click", closeMenu);
+
+
+
+// document.addEventListener("keydown", (event) => {
+//   if (event.code === "Escape") {
+//     closeMenu();
+//     console.log(event);
+//   }
+// });
+
+// menuBurger.addEventListener("click", function(){
+//   if(menuBurger){
+//     navMedia.classList.add("block");
+//     menuOpen.style.display = "none";
+//     menuClose.style.display="block"
+//     console.log("Open");
+//   } else {
+//     menuOpen.style.display = "block";
+//     menuClose.style.display="none"
+//   }
+// });
+
